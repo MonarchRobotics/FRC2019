ad6cd9432b83a1046e378f870e232a2b2ab0fbd7
jkmosa01 <35510468+jkmosa01@users.noreply.github.com>
2019-01-15 15:43:51 -0700

    I did some stuff, not sure if any of it works though lol

---
+++ src/main/java/frc/robot/Map.java
@@ -0,0 +1,22 @@
+package frc.robot;
+
+public class Map{
+    //Drive motors
+    private static int frontLeft = 1;
+    private static int frontRight = 2;
+    private static int backLeft = 3;
+    private static int backRight = 4;
+
+    public static int getBackLeft() {
+        return backLeft;
+    }
+    public static int getBackRight() {
+        return backRight;
+    }
+    public static int getFrontLeft() {
+        return frontLeft;
+    }
+    public static int getFrontRight() {
+        return frontRight;
+    }
+}
