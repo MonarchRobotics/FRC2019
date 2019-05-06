4f3d2ffd571c8f39b3884659c3bb9471ec951b09
jkmosa01 <35510468+jkmosa01@users.noreply.github.com>
2019-05-06 16:29:56 -0600

    Revert "changed some stuff"
    This reverts commit f89b9a51de086ebf6d2c9fba841640f32a91e7e2.

--- src/main/java/frc/robot/commands/Lift.java
+++ src/main/java/frc/robot/commands/Lift.java
@@ -45,87 +45,77 @@ public class Lift extends Command {
   @Override
   protected void execute() {
     //childMode
-    // if(OI.button9.get() && releasedChild){
-    //   System.out.println("Changing Mode");
-    //   Robot.switchChildMode();
-    //   releasedChild = false;
-    // }
-    // else if(!OI.button9.get()){
-    //   releasedChild = true;
-    // }
+    if(OI.button9.get() && releasedChild){
+      Robot.switchChildMode();
+      releasedChild = false;
+    }
+    else if(!OI.button9.get()){
+      releasedChild = true;
+    }
 
     int pov = OI.controller.getPOV();//gets the d-pad
-    double speed = 100.0;//the speed we want the lift to go at.
-    System.out.println(Robot.lift.getSpark().getEncoder().getPosition());
-    System.out.println(lifting);
-    System.out.println(pov);
-    System.out.println(Robot.lift.getSpark().get());
-    if(OI.controller.getBButton()){
-      Robot.lift.getSpark().set(0.5);
+    double speed = 1.0;//the speed we want the lift to go at.
+
+    if(!lifting){
+      if((pov==270 || OI.button3.get()) && Robot.lift.getSpark().getEncoder().getPosition()>0){//Checks if the left button is pressed, and if the lift isn't already at it's lowest level.
+        Robot.lift.getSpark().set(-speed);
+        lifting = true;
+        liftingTo = 0;
+      }
+      else if((pov==90 || OI.button6.get())){//right button, 2rd level.
+        if(liftingTo==2){//checks if we're at 3rd level, if so, then go down
+          Robot.lift.getSpark().set(-speed);
+          secondDirection = false;
+        }
+        else{//If we're at 1st level, go up.
+          Robot.lift.getSpark().set(speed);
+          secondDirection = true;
+        }
+        lifting = true;
+        liftingTo = 1;
+      }
+      else if((pov==0 || OI.button5.get()) && Robot.lift.getSpark().getEncoder().getPosition()<third*36 && !Robot.getChildMode()){//Top button on D-pad, and makes sure the lift isn't already at third level.
+        Robot.lift.getSpark().set(speed);  
+        lifting = true;
+        liftingTo = 2;
+      }
+      else if(OI.button2.get()){
+        if(liftingTo>0.5){
+          Robot.lift.getSpark().set(-speed);
+          climbDirection = true;
+        }
+        else{
+          Robot.lift.getSpark().set(speed);
+          climbDirection = false;
+        }
+        lifting = true;
+        liftingTo = 0.5;
+      }
+    }
+    else if(pov==180 || OI.button4.get()){//bottom of d-pad, stops the lift.
+      stopMoving();
+      System.out.println("Emergency Stop");
     }
     else{
-      Robot.lift.getSpark().set(0.0);
-    }
-    // if(!lifting){
-    //   if((pov==270 || OI.button3.get()) && Robot.lift.getSpark().getEncoder().getPosition()>0){//Checks if the left button is pressed, and if the lift isn't already at it's lowest level.
-    //     Robot.lift.getSpark().set(-speed);
-    //     lifting = true;
-    //     liftingTo = 0;
-    //   }
-    //   else if((pov==90 || OI.button6.get())){//right button, 2rd level.
-    //     if(liftingTo==2){//checks if we're at 3rd level, if so, then go down
-    //       Robot.lift.getSpark().set(-speed);
-    //       secondDirection = false;
-    //     }
-    //     else{//If we're at 1st level, go up.
-    //       Robot.lift.getSpark().set(speed);
-    //       secondDirection = true;
-    //     }
-    //     lifting = true;
-    //     liftingTo = 1;
-    //   }
-    //   else if((pov==0 || OI.button5.get()) && Robot.lift.getSpark().getEncoder().getPosition()<third*36){//Top button on D-pad, and makes sure the lift isn't already at third level.
-    //     Robot.lift.getSpark().set(speed);  
-    //     lifting = true;
-    //     liftingTo = 2;
-    //   }
-    //   else if(OI.button2.get()){
-    //     if(liftingTo>0.5){
-    //       Robot.lift.getSpark().set(-speed);
-    //       climbDirection = true;
-    //     }
-    //     else{
-    //       Robot.lift.getSpark().set(speed);
-    //       climbDirection = false;
-    //     }
-    //     lifting = true;
-    //     liftingTo = 0.5;
-    //   }
-    // }
-    // else if(pov==180 || OI.button4.get()){//bottom of d-pad, stops the lift.
-    //   stopMoving();
-    //   System.out.println("Emergency Stop");
-    // }
-    // else{
-    //   if(liftingTo==0 && Robot.lift.getSpark().getEncoder().getPosition()<=0){//has arrived at first level
-    //     stopMoving();
-    //     System.out.println("Reached 1st level");
-    //   }
-    //   else if(liftingTo==1 && ((secondDirection && Robot.lift.getSpark().getEncoder().getPosition()>=second*36) || (!secondDirection && Robot.lift.getSpark().getEncoder().getPosition()<=second*36))){//arrived at 2nd level
-    //     stopMoving();
-    //     System.out.println("Reached 2nd level");
-    //   }
-    //   else if(liftingTo==2 && Robot.lift.getSpark().getEncoder().getPosition()>=third*36){//arrived at 3rd level.
-    //     stopMoving();
-    //     System.out.println("Reached 3rd level");
-    //   }
-    //   else if(liftingTo==0.5 && ((Robot.lift.getSpark().getEncoder().getPosition()>=climb*36 && !climbDirection) || (climbDirection && Robot.lift.getSpark().getEncoder().getPosition()<=climb*36))){
-    //     stopMoving();
-    //     System.out.println("Reached Climbing Level");
-    //     System.out.println("Ready to climb to level 3");
-    //   }
+      if(liftingTo==0 && Robot.lift.getSpark().getEncoder().getPosition()<=0){//has arrived at first level
+        stopMoving();
+        System.out.println("Reached 1st level");
+      }
+      else if(liftingTo==1 && ((secondDirection && Robot.lift.getSpark().getEncoder().getPosition()>=second*36) || (!secondDirection && Robot.lift.getSpark().getEncoder().getPosition()<=second*36))){//arrived at 2nd level
+        stopMoving();
+        System.out.println("Reached 2nd level");
+      }
+      else if(liftingTo==2 && Robot.lift.getSpark().getEncoder().getPosition()>=third*36){//arrived at 3rd level.
+        stopMoving();
+        System.out.println("Reached 3rd level");
+      }
+      else if(liftingTo==0.5 && ((Robot.lift.getSpark().getEncoder().getPosition()>=climb*36 && !climbDirection) || (climbDirection && Robot.lift.getSpark().getEncoder().getPosition()<=climb*36))){
+        stopMoving();
+        System.out.println("Reached Climbing Level");
+        System.out.println("Ready to climb to level 3");
+      }
       
-    // }
+    }
     // if(OI.controller.getBButton()){
     //   Robot.lift.getSpark().set(-0.25);
     // }
@@ -151,14 +141,14 @@ public class Lift extends Command {
     return false;
   }
 
-  // Called once after isFinished returns true
-  @Override
-  protected void end() {
-  }
+  // // Called once after isFinished returns true
+  // @Override
+  // protected void end() {
+  // }
 
-  // Called when another command which requires one or more of the same
-  // subsystems is scheduled to run
-  @Override
-  protected void interrupted() {
-  }
+  // // Called when another command which requires one or more of the same
+  // // subsystems is scheduled to run
+  // @Override
+  // protected void interrupted() {
+  // }
 }
